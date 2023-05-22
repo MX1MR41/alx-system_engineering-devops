7819c769125bb445310b0d02c884f867db0bc4f2
Mohammed <107861045+MX1MR41@users.noreply.github.com>
2023-05-22 19:27:01 +0300

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,25 +1,32 @@
 #!/usr/bin/python3
-""" Script that uses JSONPlaceholder API to get information about employee """
+
 import requests
 import sys
 
+if len(sys.argv) < 2:
+    print("Please provide an employee ID")
+    sys.exit(1)
 
-if __name__ == "__main__":
-    url = 'https://jsonplaceholder.typicode.com/'
+employee_id = int(sys.argv[1])
+url = "https://jsonplaceholder.typicode.com/users/{}/todos".format(
+    employee_id)
 
-    user = '{}users/{}'.format(url, sys.argv[1])
-    res = requests.get(user)
-    json_o = res.json()
-    print("Employee {} is done with tasks".format(json_o.get('name')), end="")
+response = requests.get(url)
 
-    todos = '{}todos?userId={}'.format(url, sys.argv[1])
-    res = requests.get(todos)
-    tasks = res.json()
-    l_task = []
-    for task in tasks:
-        if task.get('completed') is True:
-            l_task.append(task)
+if response.status_code != 200:
+    print("Error retrieving TODO list")
+    sys.exit(1)
 
-    print("({}/{}):".format(len(l_task), len(tasks)))
-    for task in l_task:
-        print("\t {}".format(task.get("title")))
+todos = response.json()
+total_tasks = len(todos)
+completed_tasks = sum(1 for todo in todos if todo.get('completed'))
+
+employee_name = requests.get("https://jsonplaceholder.typicode.com/users/{}"
+                             .format(employee_id)).json().get('name')
+
+print("Employee {} is done with tasks({}/{}):".format(
+    employee_name, completed_tasks, total_tasks))
+
+for todo in todos:
+    if todo.get('completed'):
+        print("\t {}".format(todo.get('title')))
